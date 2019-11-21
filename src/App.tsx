0f97c709194c44f702777861482803f283e088da
Grant Hewitt <grant_hewitt_1@hotmail.co.uk>
2019-11-21 23:46:18 +0100

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { ConferencesPage } from "./pages/ConferencesPage";
+import { Route, Switch } from "react-router-dom";
+import { SessionList } from "./components/SessionList";
 
 const client = new ApolloClient({
     link: new HttpLink({ uri: 'http://localhost:4000/' }),
@@ -12,7 +14,10 @@ export default class App extends React.Component {
     render() {
         return (
             <ApolloProvider client={client}>
-                <ConferencesPage />
+                <Switch>
+                    <Route exact path="/" component={ConferencesPage} />
+                    <Route path="/api/conference/:uniqueName" component={SessionList} />
+                </Switch>
             </ApolloProvider>
         );
     }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider, themes } from '@stardust-ui/react';
 import App from "./App";
 
 ReactDOM.render(
     <Provider theme={themes.teams}>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root'),
 );
