1341a0b3de962ff630a7fa4148832492506e8c21
Grant Hewitt <grant_hewitt_1@hotmail.co.uk>
2019-11-21 12:03:35 +0100

    Add apollo-client to make basic GraphQL query

--- src/components/ConferenceDetails.tsx
+++ src/components/ConferenceDetails.tsx
@@ -3,15 +3,15 @@ import { Venue } from '../model/Venue';
 
 interface ConferenceDetailsProps {
     name: string;
-    startDate: Date;
-    endDate: Date;
+    startDate: string;
+    endDate: string;
     venue: Venue;
 }
 
 export const ConferenceDetails: React.FunctionComponent<ConferenceDetailsProps> = ({ name, startDate, endDate, venue }) =>
     <div>
         <div>Name: {name}</div>
-        <div>Starts on: {startDate.toDateString()}</div>
-        <div>Ends on: {endDate.toDateString()}</div>
+        <div>Starts on: {startDate}</div>
+        <div>Ends on: {endDate}</div>
         <div>Venue: {venue.name} - {venue.address.addressLine}, {venue.address.city}, {venue.address.country}, {venue.address.postalCode}</div>
     </div>;
