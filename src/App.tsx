df59b233e636ffd2562ebd1d9d590a541059f825
Grant Hewitt <grant_hewitt_1@hotmail.co.uk>
2019-11-21 22:25:45 +0100

    Add startdust-ui to style components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { ConferenceList } from "./components/ConferenceList";
+import { ConferencesPage } from "./pages/ConferencesPage";
 
 const client = new ApolloClient({
     link: new HttpLink({ uri: 'http://localhost:4000/' }),
@@ -12,7 +12,7 @@ export default class App extends React.Component {
     render() {
         return (
             <ApolloProvider client={client}>
-                <ConferenceList />
+                <ConferencesPage />
             </ApolloProvider>
         );
     }
