709e8d051018c65224c40ac7838220275817b26d
Grant Hewitt <ghewitt@users.noreply.github.com>
2019-11-22 01:14:03 +0100

    Add a conference page to display all sessions (#11)
    * Add conference page to display all sessions
    
    * Remove debug line

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { ConferencesPage } from "./pages/ConferencesPage";
 import { Route, Switch } from "react-router-dom";
-import { SessionList } from "./components/SessionList";
+import { ConferencesPage } from "./pages/ConferencesPage";
+import { ConferencePage } from "./pages/ConferencePage";
 
 const client = new ApolloClient({
     link: new HttpLink({ uri: 'http://localhost:4000/' }),
@@ -16,7 +16,7 @@ export default class App extends React.Component {
             <ApolloProvider client={client}>
                 <Switch>
                     <Route exact path="/" component={ConferencesPage} />
-                    <Route path="/api/conference/:uniqueName" component={SessionList} />
+                    <Route path="/api/conference/:uniqueName" component={ConferencePage} />
                 </Switch>
             </ApolloProvider>
         );
